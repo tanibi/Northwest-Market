9431d204ddc9d592e8823398c1ffc6f368b88832
Vamshi Krishna Yellamanchili <yellamanchilivamshi@gmail.com>
2019-10-04 20:30:37 -0500

    Created an intent in startPage activity, so when the user clicks on the "CREATE AN ACCOUNT"  button it redirects to the signup activity page

--- NorthwestMarket/app/src/main/java/com/example/northwestmarket/StartPage.java
+++ NorthwestMarket/app/src/main/java/com/example/northwestmarket/StartPage.java
@@ -2,7 +2,9 @@ package com.example.northwestmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartPage extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class StartPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+    }
+        public void onClick_Signup (View view){
+        try{
+        Intent intent = new Intent(this, SignUp.class);
+        startActivityForResult(intent, 2);
+    }
+    catch(Exception e){
+
+    }
     }
 }
