96341171fe4da252c8c3b2523e191179c62e2948
Yaswanth <yarram.pegasian@gmail.com>
2019-10-04 22:13:24 -0500

    made some changes in BuyorSell.java

--- NorthwestMarket/app/src/main/java/com/example/northwestmarket/BuyorSell.java
+++ NorthwestMarket/app/src/main/java/com/example/northwestmarket/BuyorSell.java
@@ -2,7 +2,9 @@ package com.example.northwestmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BuyorSell extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class BuyorSell extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buyor_sell);
+    }
+
+    public void onClick_sell(View v) {
+
+        try {
+            Intent in = new Intent(this, SellerHome.class);
+            startActivityForResult(in, 1);
+
+        } catch (Exception e) {
+
+
+        }
+
+
     }
 }
--- NorthwestMarket/app/src/main/java/com/example/northwestmarket/SellerHome.java
+++ NorthwestMarket/app/src/main/java/com/example/northwestmarket/SellerHome.java
@@ -16,17 +16,17 @@ public class SellerHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        //FloatingActionButton fab = findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
+           // @Override
+          //  public void onClick(View view) {
+              //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+               //         .setAction("Action", null).show();
             }
-        });
-    }
+        }//);
+   // }
 
-}
+//}
