5a514da227bf1a8d8e60edcd9c9f0bc12edbdca2
Yaswanth <yash26565@gmail.com>
2019-10-05 14:11:07 -0500

    made some changes in SignUp activity.

--- NorthwestMarket/app/src/main/java/com/example/northwestmarket/SignUp.java
+++ NorthwestMarket/app/src/main/java/com/example/northwestmarket/SignUp.java
@@ -2,7 +2,9 @@ package com.example.northwestmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+    public void onClick_createAccount(View v) {
+        Intent in = new Intent(this, Login.class);
+        startActivity(in);
+    }
+    public void onClick_back(View v) {
+        Intent in = new Intent(this, StartPage.class);
+        startActivity(in);
     }
 }
