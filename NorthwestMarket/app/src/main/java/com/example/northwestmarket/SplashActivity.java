f57786e22c109f045904938199aa95d290d8b86c
Binita Singh <46681260+tanibi@users.noreply.github.com>
2019-12-02 16:31:28 -0600

    modifying spashscreen

--- NorthwestMarket/app/src/main/java/com/example/northwestmarket/SplashActivity.java
+++ NorthwestMarket/app/src/main/java/com/example/northwestmarket/SplashActivity.java
@@ -12,7 +12,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        SystemClock.sleep(3000);
+        SystemClock.sleep(4000);
         Intent login = new Intent(SplashActivity.this,MainActivity.class);
         startActivity(login);
         finish();
