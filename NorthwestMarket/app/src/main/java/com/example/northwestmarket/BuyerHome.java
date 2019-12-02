096679842b47a199c461cb46b563be8b549f2ad0
SandeepDevineni <devinenisandeep1997@gmail.com>
2019-12-02 16:52:07 -0600

    Committing Sports.java

--- NorthwestMarket/app/src/main/java/com/example/northwestmarket/BuyerHome.java
+++ NorthwestMarket/app/src/main/java/com/example/northwestmarket/BuyerHome.java
@@ -34,7 +34,7 @@ public class BuyerHome extends AppCompatActivity {
         iv1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ini=new Intent(BuyerHome.this,Details.class);
+                Intent ini=new Intent(BuyerHome.this,Sports.class);
                 startActivity(ini);
             }
         });
